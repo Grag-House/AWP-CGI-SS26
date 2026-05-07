28291bb71267d0f4df13614ae3bc01e886d5a073
Lasse <lkossira@gmail.com>
2026-05-07 12:00:48 +0200

    -strings externalised

--- app/src/main/java/hka/awp/temi_cgi_app/feature/weatherscreen/WeatherCards.kt
+++ app/src/main/java/hka/awp/temi_cgi_app/feature/weatherscreen/WeatherCards.kt
@@ -1,14 +1,18 @@
 package hka.awp.temi_cgi_app.feature.weatherscreen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import hka.awp.temi_cgi_app.R
+
 data class HourlyItem(
-    val label: String,       // "Jetzt", "t+1", …
+    var label: String,       // "Jetzt", "t+1", …
     val icon: WeatherIcon,
     val temp: String,        // "21°"
     val precipitation: String         // "0%"
 )
 
 data class DailyItem(
-    val day: String,         // "Heute", "Do", …
+    var day: String,         // "Heute", "Do", …
     val icon: WeatherIcon,
     val high: String,
     val low: String
@@ -17,21 +21,22 @@ data class DailyItem(
 class WeatherCards {
 
     companion object {
+
         fun setHourlyWeatherCards(): MutableList<HourlyItem> {
             val data = WeatherData.getHourlyData()
 
             val hourlyData = mutableListOf<HourlyItem>()
 
-            hourlyData.add(
-                HourlyItem(
-                    "Jetzt",
-                    data[0].symbol,
-                    data[0].temp.toString(),
-                    data[0].precipitation.toString()
-                )
-            )
+//            hourlyData.add(
+//                HourlyItem(
+//                    stringResource(R.string.now),
+//                    data[0].symbol,
+//                    data[0].temp.toString(),
+//                    data[0].precipitation.toString()
+//                )
+//            )
 
-            for (i in 1..(data.size - 1)) {
+            for (i in 0..(data.size - 1)) {
                 hourlyData.add(
                     HourlyItem(
                         data[i].time,
@@ -44,21 +49,22 @@ class WeatherCards {
             return hourlyData
         }
 
+
         fun setDailyWeatherCards(): MutableList<DailyItem>{
             val data = WeatherData.getWeeklyData()
 
             val dailyData = mutableListOf<DailyItem>()
 
-            dailyData.add(
-                DailyItem(
-                    "Heute",
-                    data[0].symbol,
-                    data[0].maxTemp.toString(),
-                    data[0].minTemp.toString()
-                )
-            )
+//            dailyData.add(
+//                DailyItem(
+//                    stringResource(R.string.today),
+//                    data[0].symbol,
+//                    data[0].maxTemp.toString(),
+//                    data[0].minTemp.toString()
+//                )
+//            )
 
-            for (i in 1..minOf(6,(data.size - 1))) {
+            for (i in 0..minOf(6,(data.size - 1))) {
                 dailyData.add(
                     DailyItem(
                         data[i].weekday,
