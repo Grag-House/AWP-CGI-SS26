697c7b8fd9f7cb555521f6f02f89538df767e2c5
Lasse Kossira <uuzqd@student.kit.edu>
2026-06-10 00:27:55 +0200

    -started coordinate editing

--- app/src/main/java/hka/awp/cgi/temi/app/feature/settings/adminPanel/AdminPanelViewModel.kt
+++ app/src/main/java/hka/awp/cgi/temi/app/feature/settings/adminPanel/AdminPanelViewModel.kt
@@ -6,13 +6,23 @@ import hka.awp.cgi.temi.app.feature.weatherscreen.WeatherRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlin.math.round
 
 class AdminPanelViewModel : ViewModel() {
+
     private val _uiState = MutableStateFlow(AdminPanelState())
     val uiState: StateFlow<AdminPanelState> = _uiState.asStateFlow()
 
-    fun onEditCoordinates() {
-        TODO()
+
+    fun onEditCoordinates(coordinates: AdminPanelState) {
+        if(coordinates.longitude > 0 && coordinates.latitude > 0){
+            WeatherRepository.LONGITUDE = round(coordinates.longitude * 10000) / 10000
+            WeatherRepository.LATITUDE = round(coordinates.latitude * 10000) / 10000
+        }
+        else {
+            WeatherRepository.LONGITUDE = 8.3573
+                WeatherRepository.LATITUDE = 49.0138
+        }
     }
 
     fun onOpenMqttReports() {
@@ -30,5 +40,9 @@ data class AdminPanelState(
 
     val appVersion: String = BuildConfig.VERSION_NAME,
 
-    val coordinates: String = "Länge: ${WeatherRepository.LATITUDE} Breite: ${WeatherRepository.LONGITUDE}"
-)
+    val coordinates: String = "Länge: ${WeatherRepository.LONGITUDE} Breite: ${WeatherRepository.LATITUDE}",
+
+    var longitude: Double = 0.0,
+
+    var latitude: Double = 0.0
+                          )
