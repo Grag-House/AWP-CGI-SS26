52df3ec1c2c8279ae0cd80c6a9a28f3f6a34b89d
Lasse Kossira <uuzqd@student.kit.edu>
2026-06-09 20:28:09 +0200

    -started fetching for coordinates and app version and webserver url

--- app/src/main/java/hka/awp/cgi/temi/app/feature/settings/adminPanel/AdminPanelViewModel.kt
+++ app/src/main/java/hka/awp/cgi/temi/app/feature/settings/adminPanel/AdminPanelViewModel.kt
@@ -1,12 +1,40 @@
 package hka.awp.cgi.temi.app.feature.settings.adminPanel
 
 import androidx.lifecycle.ViewModel
+import hka.awp.cgi.temi.app.BuildConfig
+import hka.awp.cgi.temi.app.feature.weatherscreen.WeatherRepository
 import hka.awp.cgi.temi.app.feature.weatherscreen.WeatherState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class AdminPanelViewModel : ViewModel() {
-    private val _uiState = MutableStateFlow(WeatherState())
-    val uiState: StateFlow<WeatherState> = _uiState.asStateFlow()
+    private val _uiState = MutableStateFlow(AdminPanelState())
+    val uiState: StateFlow<AdminPanelState> = _uiState.asStateFlow()
+
+    fun onEditCoordinates(){
+        TODO()
+    }
+
+    fun onOpenCoordinates() {
+        TODO()
+    }
+
+    fun onOpenMqttReports() {
+        TODO()
+    }
+
+    fun onChangePassword(){
+        TODO()
+    }
+
 }
+
+data class AdminPanelState(
+
+    val webserverUrl: String = "",
+
+    val appVersion: String = BuildConfig.VERSION_NAME,
+
+    val coordinates: String = "Latittude: ${WeatherRepository.LATITUDE} Longitude: ${WeatherRepository.LONGITUDE}"
+                          )
